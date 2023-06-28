17e0242908dacd7792250961e1d5d8ca50acb619
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-28 19:56:20 +0900

    init commit

---
+++ src/main/java/com/sparta/springauto/SpringAutoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAutoApplication.class, args);
+    }
+
+}
